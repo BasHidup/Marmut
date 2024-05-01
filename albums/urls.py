83d243b7f0cb4fb570ff4dabda91861bb50cee0b
Alifa Hanania Ardha <alifa.hanania@ui.ac.id>
2024-05-01 22:34:29 +0700

    CRUD Kelola Album & Song dan RD Album song (label pov) done

--- albums/urls.py
+++ albums/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from albums.views import create_song, edit_album, show_albums, create_album, show_songs
+from albums.views import create_song, edit_album, show_albums, create_album, show_songs, show_song_detail, edit_song
 
 app_name = 'albums'
 
@@ -9,4 +9,6 @@ urlpatterns = [
     path('<str:id_album>/songs/', show_songs, name='show_songs'),
     path('<str:id_album>/create-song/', create_song, name='create_song'),
     path('<str:id_album>/edit-album/', edit_album, name='edit_album'),
+    path('song-detail/<str:id_song>/', show_song_detail, name='show_song_detail'),
+    path('edit-song/<str:id_song>/', edit_song, name='edit_song'),
 ]
